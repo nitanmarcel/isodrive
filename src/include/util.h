56b8356b6429ef985635dd0cac32480783f195a8
XeonDead <xeondead@gmail.com>
2023-06-01 12:55:01 +0300

    isodrive: uncrustify code to google cpp writing style

--- src/include/util.h
+++ src/include/util.h
@@ -1,11 +1,12 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-char *fs_mount_point(char *filesystem_type);
-char *strjin(char *w1, char *w2);
-bool isdir(char *path);
-char *sysfs_read(char *path);
-void sysfs_write(char *path, char *content);
+char* fs_mount_point(char *filesystem_type);
+char* strjin(char *w1,
+             char *w2);
+bool  isdir(char *path);
+char* sysfs_read(char *path);
+void  sysfs_write(char *path,
+                  char *content);
 
-#endif
-
+#endif // ifndef UTIL_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,59 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-		char *iso_target = argv[1];
-		char *cdrom = (char*)"0";
-		char *ro = (char*)"1";
-		for (int i = 1; i < argc; i++)
-		{
-			if (strcmp(argv[i], "-rw") == 0)
-			{
-				ro = (char*)"0";
-			}
-			else if (strcmp(argv[i], "-cdrom") == 0)
-			{
-				cdrom = (char*)"1";
-			}
-			else
-			{
-				iso_target = argv[i];
-			}
-		}
+int main(int argc, char *argv[])
+{
+  char *iso_target = argv[1];
+  char *cdrom      = (char *)"0";
+  char *ro         = (char *)"1";
 
-		if (argc == 1)
-		{
-			printf("Usage:\n");
-			printf("cdrom [FILE]... [OPTION]...\n");
-			printf("Mounts the given FILE as a bootable device using configfs.\n");
-			printf("Run without any arguments to unmount any mounted files and display this help message.\n\n");
+  for (int i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "-rw") == 0)
+    {
+      ro = (char *)"0";
+    }
+    else if (strcmp(argv[i], "-cdrom") == 0)
+    {
+      cdrom = (char *)"1";
+    }
+    else
+    {
+      iso_target = argv[i];
+    }
+  }
 
-			printf("Optional arguments:\n");
-			printf("-rw\t Mounts the file in read write mode.\n");
-			printf("-cdrom\t Mounts the file as a cdrom.\n\n");
-			printf("UMOUNT:\n");
-		}
-		else
-		{
-			printf("MOUNT:\n");
-		}
+  if (argc == 1)
+  {
+    printf("Usage:\n");
+    printf("cdrom [FILE]... [OPTION]...\n");
+    printf("Mounts the given FILE as a bootable device using configfs.\n");
+    printf(
+      "Run without any arguments to unmount any mounted files and display this help message.\n\n");
 
-	if (!supported())
-	{
-		printf("Device does not support configfs usb gadget\n");
-		return 1;
-	}
-	if (getuid() != 0)
-	{
-		printf("Permission denied\n");
-		return 1;
-	}
+    printf("Optional arguments:\n");
+    printf("-rw\t Mounts the file in read write mode.\n");
+    printf("-cdrom\t Mounts the file as a cdrom.\n\n");
+    printf("UMOUNT:\n");
+  }
+  else
+  {
+    printf("MOUNT:\n");
+  }
 
-	mount_iso(iso_target, cdrom, ro);
+  if (!supported())
+  {
+    printf("Device does not support configfs usb gadget\n");
+    return 1;
+  }
 
-	return 0;
+  if (getuid() != 0)
+  {
+    printf("Permission denied\n");
+    return 1;
+  }
+
+  mount_iso(iso_target, cdrom, ro);
+
+  return 0;
 }
